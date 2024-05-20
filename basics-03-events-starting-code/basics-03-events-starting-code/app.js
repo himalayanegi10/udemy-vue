dfbcc99ec5cb670603c13abea700e7acf59a40b8
himalayanegi10 <himalayanegi10@gmail.com>
2024-05-21 00:44:56 +0530

    Udemy-Vue: #2 Till 2.28

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -1,9 +1,30 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 8,
+      name: "",
+      finalName: "",
     };
+  },
+  methods: {
+    add(num) {
+      // this.counter++;
+      this.counter += num;
+    },
+    sub(num) {
+      // --this.counter;
+      this.counter -= num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm(event) {
+      alert("Submitted");
+    },
+    confirmName() {
+      this.finalName = this.name;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
