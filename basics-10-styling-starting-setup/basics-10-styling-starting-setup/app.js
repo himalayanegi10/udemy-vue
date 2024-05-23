ddde3ceed1ea211524f1a81e4200d4ed0742e0e5
himalayanegi10 <himalayanegi10@gmail.com>
2024-05-24 02:18:04 +0530

    Classes and computed properties

--- basics-10-styling-starting-setup/basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/basics-10-styling-starting-setup/app.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+  },
   methods: {
     selectBox(value) {
       if (value === "A") {
