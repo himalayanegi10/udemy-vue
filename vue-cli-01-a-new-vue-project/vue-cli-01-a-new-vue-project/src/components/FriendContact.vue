c0e8ba70fec4b9bd2539b347435b25b540d3bced
himalayanegi10 <himalayanegi10@gmail.com>
2024-06-16 21:58:43 +0530

    Using Vue components

---
+++ vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2> {{ friend.name }} </h2>
+    <button @click="toggleDetailVisible"> {{ isDetailVisible ? 'Show' : 'Hide'}} Details </button>
+    <ul v-if="isDetailVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isDetailVisible: false,
+        friend: {
+          id: "Himalaya",
+          name: "Himalaya",
+          phone: "1234 567 899",
+          email: "himalaya@gmail.com",
+        }
+      };
+    },
+    methods: {
+      toggleDetailVisible(){
+        this.isDetailVisible = !this.isDetailVisible;
+      }
+    }
+  };
+</script>
