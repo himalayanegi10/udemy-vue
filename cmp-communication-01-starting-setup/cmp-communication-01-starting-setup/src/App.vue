7aa3c783f16c8c4f45194f4d88eb4c8061f0a05e
himalayanegi10 <himalayanegi10@gmail.com>
2024-07-09 01:28:40 +0530

    New Friend: Adding component and connecting them

--- cmp-communication-01-starting-setup/cmp-communication-01-starting-setup/src/App.vue
+++ cmp-communication-01-starting-setup/cmp-communication-01-starting-setup/src/App.vue
@@ -3,6 +3,9 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <add-new-contact
+        @add-friend="addFriend"
+    />
     <ul>
       <friend-contact
           v-for="friend in friends"
@@ -44,6 +47,9 @@ export default {
     toggleFavorite(formID){
       const toggleFavoriteForFriend = this.friends.find(f => f.id === formID);
       toggleFavoriteForFriend.favorite = !toggleFavoriteForFriend.favorite;
+    },
+    addFriend(friend){
+      this.friends.push(friend);
     }
   }
 };
@@ -75,7 +81,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -104,5 +111,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ cmp-communication-01-starting-setup/cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <form @submit.prevent="addFriend">
+      <div>
+        <input type="text" placeholder="Name" v-model="name" />
+      </div>
+      <div>
+        <input type="email" placeholder="Email" v-model="email" />
+      </div>
+      <div>
+        <input type="tel" placeholder="Phone" v-model="phone" />
+      </div>
+      <div>
+        <button type="submit">Add New Friend</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: null,
+      email: null,
+      phone: null,
+      favorite: false
+    };
+  },
+  emits: ['add-friend'],
+  computed: {
+    id: function () {
+      return (this.name + "-" + this.email);
+    }
+  },
+  methods: {
+    addFriend(){
+      this.$emit("add-friend", {
+        id: this.id,
+        name: this.name,
+        email: this.email,
+        phone: this.phone,
+        favorite: this.favorite
+      });
+    }
+  }
+};
+</script>
